de6b1b80f178060c80ba8ce7e8b4a2b882418a86
RockRakib <41679512+RockRakib@users.noreply.github.com>
2023-11-29 23:25:06 +0600

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,62 @@
+{
+  // editor
+  "editor.fontSize": 20,
+  "editor.fontWeight": "50",
+  "editor.fontamily": "'Fira Code iScript', 'Fira Code', Consolas, 'Courier New', monospace",
+  "editor.fontLigatures": true,
+  "editor.wordWrap": "on",
+  "editor.minimap.enabled": false,
+  "editor.tokenColorCustomizations": {
+    "textMateRules": [
+      {
+        "scope": "comment",
+        "settings": {
+          "fontStyle": "italic"
+          // "font-family": "JetBrains Mono"
+        }
+      },
+      {
+        "scope": "variable.other",
+        "settings": {
+          "fontStyle": "italic"
+        }
+      }
+      // {
+      //   "scope": "variable.",
+      //   "settings": {
+      //     "fontStyle": "italic"
+      //     // "font-family": "JetBrains Mono"
+      //   }
+      // }
+    ]
+  },
+  // cursor
+  "editor.cursorSmoothCaretAnimation": "on",
+  "editor.cursorBlinking": "expand",
+  // config related to code formatting
+  "editor.defaultFormatter": "esbenp.prettier-vscode",
+  "editor.formatOnSave": true,
+  "[javascript]": {
+    "editor.formatOnSave": false,
+    "editor.defaultFormatter": null
+  },
+  "[javascriptreact]": {
+    "editor.formatOnSave": false,
+    "editor.defaultFormatter": null
+  },
+  "editor.codeActionsOnSave": {
+    "source.fixAll.eslint": true,
+    "source.fixAll.tslint": true,
+    "source.organizeImports": true
+  },
+  "eslint.alwaysShowStatus": true,
+  //terminal
+  "terminal.integrated.fontSize": 16,
+  "terminal.integrated.fontWeight": "normal",
+  "terminal.integrated.fontFamily": "Fira Code",
+  "workbench.colorTheme": "Learn with Sumit - Vintage",
+  "workbench.iconTheme": "vscode-icons",
+  // terminal customization
+
+  "editor.fontFamily": "'Fira Code iScript', 'Fira Code', Consolas, 'Courier New', monospace"
+}
